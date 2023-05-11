10d38df52959b17205708fc1670cb609f687b4c7
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2023-05-11 22:56:38 +0400

    kardineUpdate

--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -12,40 +12,44 @@ const Portfolio = () => {
 
         {
             id: 1,
-            img: multiShoop,
-            title: 'Multi Shopp',
-            github: "https://github.com/SerjAyvazyan-2000/Multi-Shoop",
-            demo: 'https://serjayvazyan-2000.github.io/Multi-Shoop'
+            img: kardone,
+            title: 'Kardone Shopp',
+            github: "https://github.com/SerjAyvazyan-2000/Kardone-Shoop",
+            demo: 'https://serjayvazyan-2000.github.io/Kardone-Shoop',
+            process: "Is In Process",
         },
         {
             id: 2,
             img: memoryGame,
             title: 'Memory Game',
             github: "https://github.com/SerjAyvazyan-2000/Memory-Game",
-            demo: 'https://serjayvazyan-2000.github.io/Memory-Game'
+            demo: 'https://serjayvazyan-2000.github.io/Memory-Game',
+
         },
         {
             id: 3,
             img: school,
             title: "School",
             github: "https://github.com/SerjAyvazyan-2000/Admin-School",
-            demo: 'https://serjayvazyan-2000.github.io/Admin-School'
+            demo: 'https://serjayvazyan-2000.github.io/Admin-School',
+
         },
 
         {
             id: 4,
-            img: kardone,
-            title: 'Kardone Shopp',
-            github: "https://github.com/SerjAyvazyan-2000/Kardone-Shoop",
-            demo: 'https://serjayvazyan-2000.github.io/Kardone-Shoop',
-            process: "Is In Process"
+            img: multiShoop,
+            title: 'Multi Shopp',
+            github: "https://github.com/SerjAyvazyan-2000/Multi-Shoop",
+            demo: 'https://serjayvazyan-2000.github.io/Multi-Shoop',
+
         },
         {
-            id: 4,
+            id: 5,
             img: lessons,
             title: 'My Home Work',
             github: "https://github.com/SerjAyvazyan-2000/Home-Work",
             demo: 'https://serjayvazyan-2000.github.io/Home-Work/',
+
         }
     ]
 
@@ -62,7 +66,9 @@ const Portfolio = () => {
                                 <img src={item.img} alt="multiShoop"/>
                             </div>
                             <h3>{item.title}</h3>
-                            {item.process ? <p>{item.process}</p> : null}
+                            {item.title === 'Kardone Shopp' ?
+                                <span>&#11088;&#11088;&#11088;&#11088;</span>
+                            :null}
                             <div className="portfolio-item-cta">
                                 <a href={item.github} className="btn">GitHub</a>
                                 <a href={item.demo} className="btn btn-primaly" target="_blank">Live Demo</a>
